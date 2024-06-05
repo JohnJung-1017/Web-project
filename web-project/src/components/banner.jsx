466108e73a1e2642901c4c36365801897b131b06
JohnJung-1017 <jungih1017@gmail.com>
2024-06-05 22:44:08 +0900

    feat: banner.jsx
    navBar 로그아웃 기능 구현

--- web-project/src/components/banner.jsx
+++ web-project/src/components/banner.jsx
@@ -1,24 +1,37 @@
 import React from 'react'
-import { Link } from 'react-router-dom'
+import { Link, useNavigate } from 'react-router-dom'
 import { Icon } from '@iconify/react';
+import { getAuth, signOut } from 'firebase/auth';
 import '../css/banner.css';
 
-function banner() {
+function Banner() {
+  const navigate = useNavigate();
+  const auth = getAuth();
+
+  const handleLogout = async () => {
+    try {
+      await signOut(auth);
+      navigate('/');
+    } catch (error) {
+      console.error('Error logging out: ', error);
+    }
+  };
+
   return (
     <div>
         <div className='banner'>
             <div className='banner_title'>
                 <Icon icon="unjs:jiti" width="6vh" height="6vh" />
-                <p>오늘     뭐먹지?</p>
+                <p>오늘 뭐먹지?</p>
             </div>
             <Link className='bannerMenu' to={'/main'}>Home</Link>
             <Link className='bannerMenu' to={'/board'}>게시판</Link>
             <Link className='bannerMenu' to={'/recommend'}>추천메뉴</Link>
             <Link className='bannerMenu' to={'/search'}>검색</Link>
-            <button className="logout_button" onClick={null}>logout</button>
+            <button className="logout_button" onClick={handleLogout}>logout</button>
         </div>
     </div>
   )
 }
 
-export default banner
+export default Banner
