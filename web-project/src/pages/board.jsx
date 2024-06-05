30f2d135f4faf70869eb75bd7f19ff4227974b69
JohnJung-1017 <jungih1017@gmail.com>
2024-06-05 22:46:16 +0900

    feat: modal.css
    css 수정 및 각 페이지 적용

--- web-project/src/pages/board.jsx
+++ web-project/src/pages/board.jsx
@@ -1,12 +1,17 @@
 import React from 'react'
-import Banner from './banner';
+import Banner from '../components/banner';
+import '../css/board.css';
 
 const board = () => {
   return (
     <div className='main_background'>
       <Banner/>
       <div className='main_content'>
-        무야호
+        <div>
+          <h3 className='main_title'>[자유 게시판]</h3>
+          <hr/>
+          <div className='main_scroll_box'></div>
+        </div>
       </div>
     </div>
   )
--- web-project/src/pages/main.jsx
+++ web-project/src/pages/main.jsx
@@ -1,5 +1,6 @@
 import React from 'react'
-import Banner from './banner';
+import Banner from '../components/banner';
+import "../css/main.css";
 
 const Main = () => {
   return (
